c881225120efbf6d9987db26083628f07c18b0da
jinchi.lu <jinchilu@hotmail.com>
2012-07-09 22:20:24 +0200

    Initial Commit

---
+++ StoryboardTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StoryboardTest
+//
+//  Created by chang on 12-7-9.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StoryboardTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StoryboardTest
+//
+//  Created by chang on 12-7-9.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
